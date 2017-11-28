6eabd5d55b5707d6ff427ce13e509dbe001a624f
Sean Griffin <sean@seantheprogrammer.com>
2017-11-28 15:42:43 -0700

    Update for 0.99.0

--- config.rb
+++ config.rb
@@ -40,7 +40,7 @@ helpers do
   end
 
   def repo_url(path)
-    "https://github.com/diesel-rs/diesel/tree/v0.16.0/#{path}"
+    "https://github.com/diesel-rs/diesel/tree/v0.99.0/#{path}"
   end
 
   def example_file(path)
