2f75504eee94dd1ccdd8f226494fdfdb59ff5394
Sean Griffin <sean@seantheprogrammer.com>
2017-12-23 14:43:47 -0700

    Update to Diesel 1.0.0-rc1

--- config.rb
+++ config.rb
@@ -40,7 +40,7 @@ helpers do
   end
 
   def repo_url(path)
-    "https://github.com/diesel-rs/diesel/tree/v1.0.0-beta1/#{path}"
+    "https://github.com/diesel-rs/diesel/tree/v1.0.0-rc1/#{path}"
   end
 
   def example_file(path)
