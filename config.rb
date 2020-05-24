32218f87c3ec5bce0dca3d98fe5ab08078e5e409
Issy Long <me@issyl0.co.uk>
2020-05-24 17:24:10 +0100

    Use diesel version 1.4.4 in example & GitHub links
    - I copied and pasted the Getting Started examples, and the Rust plugin
      from my editor told me that the versions were out of date. Turns out
      that the GitHub links pointed to tag 1.3.0 not the latest, 1.4.4.
    - Keeping these up to date is a little bit of a maintenance burden, but
      I couldn't find such thing as a "latest" tag.

--- config.rb
+++ config.rb
@@ -40,7 +40,7 @@ helpers do
   end
 
   def repo_url(path)
-    "https://github.com/diesel-rs/diesel/tree/v1.3.0/#{path}"
+    "https://github.com/diesel-rs/diesel/tree/v1.4.4/#{path}"
   end
 
   def example_file(path)
