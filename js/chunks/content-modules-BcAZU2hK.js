c61a3a99750608ef05d968889643804058368558
weiznich <github@weiznich.de>
2026-03-27 09:05:08 +0000

    Deploying to gh-pages from @ sgrif/diesel.rs-website@c5feefc647adf86982605794cb9423a241ac0689 🚀

--- js/chunks/content-modules-BcAZU2hK.js
+++
@@ -1,6 +0,0 @@
-const contentModules = new Map([
-["src/content/docs/api_docs/index.mdx", () => import('./index-c2UHJa-J.js')],
-["src/content/docs/index.mdx", () => import('./index-D7veiABC.js')],
-["src/content/docs/guides/getting-started.mdx", () => import('./getting-started-Csi8101m.js')]]);
-
-export { contentModules as default };
