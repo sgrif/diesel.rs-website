7e17374042cfd3f0641527466d3b63fe34a6b326
weiznich <github@weiznich.de>
2026-05-12 11:48:04 +0000

    Deploying to gh-pages from @ sgrif/diesel.rs-website@d6474a49409231f759402f61be4a85b7fa468951 🚀

--- js/chunks/collection-config-B8MAap3_.js
+++
@@ -1,7 +0,0 @@
-let userCollections;
-			try {
-				userCollections = (await import('./content.config-CoI18LSM.js')).collections;
-			} catch {}
-			const collections = userCollections;
-
-export { collections };
