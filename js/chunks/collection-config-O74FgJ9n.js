25ded2538c999575c1a3a9c72c8e297a69b091bb
weiznich <github@weiznich.de>
2026-07-24 05:27:10 +0000

    Deploying to gh-pages from @ sgrif/diesel.rs-website@185936b509eb77eb7576bb9bb2c5c3c8e70fda8e 🚀

--- js/chunks/collection-config-O74FgJ9n.js
+++
@@ -1,7 +0,0 @@
-let userCollections;
-			try {
-				userCollections = (await import('./content.config-DxWErxfP.js')).collections;
-			} catch {}
-			const collections = userCollections;
-
-export { collections };
